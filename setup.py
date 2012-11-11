7e761a15f5e72bdd947dbb8e0f645a96bd3d8b67
Ronald Evers <ronald@ch10.nl>
2012-11-11 22:25:27 +0100

    add twisted dependency, move module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='botlerplate',
-    packages=['botlerplate'],
+    py_modules=['botlerplate'],
     version='0.1',
     author='Ronald Evers',
     author_email='ronald@ch10.nl',
@@ -15,4 +15,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'],
+    install_requires=['twisted'],
 )
