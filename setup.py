d329b85580060a294288257be4bcb0b4da117ae4
Ronald Evers <ronald@ch10.nl>
2012-11-11 23:03:07 +0100

    fix openssl dependency

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    install_requires=['twisted'],
+    install_requires=['twisted',
+                      'pyopenssl'],
 )
